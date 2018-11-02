030610401d7287c1c7dab0f6770b1e976c635606
kushal <kushaldeveloper@gmail.com>
2018-11-02 11:29:03 -0400

    add semicolon
    https://gitlab-artifacts.s3.amazonaws.com/83/39/833946a9632ef09f5ca8a011960013b2569a185d241755caace9c271a7ca0a5c/2018_11_02/115808353/108205609/job.log?response-content-type=text/plain%3B%20charset%3Dutf-8&response-content-disposition=inline&X-Amz-Expires=600&X-Amz-Date=20181102T152845Z&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJTOFWQ3GL4O3Q3FA/20181102/us-east-1/s3/aws4_request&X-Amz-SignedHeaders=host&X-Amz-Signature=3bb3a3c5255d697480e4c60f23e4e2b1e8a06283a8ba2ec8b23cbead54d6a815
    
    C:\Users\hadak\source\repos\mymcny\database\create_db.sql
    Error:(1, 8) expected one of the following: EDITIONING FORCE FUNCTION NO OR PACKAGE PROCEDURE SEQUENCE TRIGGER TYPE VIEW identifier
    Error:(1, 42) expected:
    Error:(2, 8) expected one of the following: EDITIONING FORCE FUNCTION NO OR PACKAGE PROCEDURE SEQUENCE TRIGGER TYPE VIEW identifier
    Error:(2, 33) expected:
    Error:(4, 8) expected one of the following: EDITIONING FORCE FUNCTION NO OR PACKAGE PROCEDURE SEQUENCE TRIGGER TYPE VIEW identifier
    Error:(4, 20) expected:
    Error:(5, 37) expected one of the following: ( SELECT WITH
    Error:(9, 8) expected one of the following: EDITIONING FORCE FUNCTION NO OR PACKAGE PROCEDURE SEQUENCE TRIGGER TYPE VIEW identifier
    Error:(9, 20) expected:
    Error:(11, 46) expected one of the following: ( SELECT WITH

--- database/create_db.sql
+++ database/create_db.sql
@@ -1,11 +1,13 @@
 create extension if not exists "pgcrypto";
 create schema if not exists mcny;
+
 create table person (
     id uuid default gen_random_uuid(),
     legal_name varchar(400)
-)
+);
+
 create table videos (
     id uuid,
     timestamp without time zone default (now() at time zone 'utc'),
     payload jsonb
-)
+);
