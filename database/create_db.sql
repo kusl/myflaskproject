9fd30d7e740ee74d442ff37c0d04aef7a5b1d86e
kushal <kushaldeveloper@gmail.com>
2018-11-02 14:39:14 -0400

    add namespace schema to tables

--- database/create_db.sql
+++ database/create_db.sql
@@ -1,12 +1,12 @@
 create extension if not exists "pgcrypto";
 create schema if not exists mcny;
 
-create table person (
+create table mcny.person (
     id uuid default gen_random_uuid(),
     legal_name varchar(400)
 );
 
-create table videos (
+create table mcny.videos (
     id uuid,
     timestamp without time zone default (now() at time zone 'utc'),
     payload jsonb
