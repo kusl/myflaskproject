767c53e5fed0fdf10b165fb3387cca91cb119f47
kushal <kushaldeveloper@gmail.com>
2018-11-02 21:21:45 -0400

    use defaults
    C:\Users\hadak\source\repos\mymcny\database\create_db.sql
    Error:(1, 8) expected one of the following: EDITIONING FORCE FUNCTION NO OR PACKAGE PROCEDURE SEQUENCE TRIGGER TYPE VIEW identifier
    Error:(1, 42) expected:
    Error:(2, 8) expected one of the following: EDITIONING FORCE FUNCTION NO OR PACKAGE PROCEDURE SEQUENCE TRIGGER TYPE VIEW identifier
    Error:(2, 33) expected:
    Error:(4, 8) expected one of the following: EDITIONING FORCE FUNCTION NO OR PACKAGE PROCEDURE SEQUENCE TRIGGER TYPE VIEW identifier
    Error:(4, 24) expected:
    Error:(5, 37) expected one of the following: ( SELECT WITH
    Error:(9, 8) expected one of the following: EDITIONING FORCE FUNCTION NO OR PACKAGE PROCEDURE SEQUENCE TRIGGER TYPE VIEW identifier
    Error:(9, 25) expected:
    Error:(10, 37) expected one of the following: ( SELECT WITH
    Error:(11, 57) expected one of the following: ( SELECT WITH

--- database/create_db.sql
+++ database/create_db.sql
@@ -7,7 +7,7 @@ create table mcny.users (
 );
 
 create table mcny.videos (
-    id uuid,
+    id uuid default gen_random_uuid(),
     video_time timestamp without time zone default (now() at time zone 'utc'),
     payload jsonb
 );
